dc5d963173a7f907e41d92284b624e41f0c1be38
fazza-tech <ebranfazza@gmail.com>
2024-12-21 20:48:48 +0530

    updated

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main(){
+    return(
+        <main>
+            <h1>Fun Facts About Vite 🚀</h1>
+            <ul className="list-info">
+                <li>Name: "Vite" means fast in French, highlighting its speed.</li>
+                <li>HMR: Instant Hot Module Replacement makes development ultra-responsive.</li>
+                <li>ES Modules: Uses native ES Modules for faster startup in dev mode.</li>
+                <li>Modern Frameworks: Works great with Vue, React, Svelte, and more.</li>
+                <li>Multi-Framework: Initially for Vue, now supports almost any JS framework.</li>
+            </ul>
+        </main>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+export default function Navbar(){
+    return(
+        <>
+            <header>
+                <nav>
+                    <img src="vite.svg" alt="vite logo" />
+                    <span>ViteFacts</span>
+                </nav>
+            </header>
+        </>
+    )
+}
